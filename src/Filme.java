38ede8e3c93dc0c6517ee84fec1142a02d4d249a
Bruno Lins Maia <brunolinsm@gmail.com>
2024-03-22 14:13:05 -0300

    Configurando o que pode ser visto e configurado

---
+++ src/Filme.java
@@ -0,0 +1,22 @@
+public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliaçoes;
+    int duracaoEmMinutos;
+
+    void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+
+    void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliaçoes++;
+    }
+
+    double exibeMediaDasAvalicoes() {
+        return somaDasAvaliacoes / totalDeAvaliaçoes;
+    }
+}
